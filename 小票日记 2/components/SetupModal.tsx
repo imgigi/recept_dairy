fd8ffdc269c9083e4cc1697db005705299707125
gigishithub <128673757+imgigi@users.noreply.github.com>
2026-02-28 14:59:38 +0800

    Delete 小票日记 2 directory

--- 小票日记 2/components/SetupModal.tsx
+++
@@ -1,4 +0,0 @@
-// This file is deprecated and replaced by BudgetManager.tsx
-// Keeping file empty or returning null to avoid build errors if imported elsewhere, 
-// though we will remove imports in App.tsx.
-export default function SetupModal() { return null; }
